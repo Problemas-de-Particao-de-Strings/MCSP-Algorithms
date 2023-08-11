8dc9c6d1a2df72332b8b2036e436e188233a74c5
Tiago de Paula <tiagodepalves@gmail.com>
2023-08-11 03:11:20 -0300

    style(random): rename to RandomIO
    Also, adds example for `mkWord64`.

--- src/Strings/System/Random/Static.hs
+++ src/Strings/System/Random/Static.hs
@@ -1,6 +1,6 @@
 module Strings.System.Random.Static (
     mkWord64,
-    Random,
+    RandomIO,
     mkRandomQ,
     mkRandom,
     mkRandomE,
@@ -13,21 +13,24 @@ import System.Random.PCG.Class (uniformW64)
 import Language.Haskell.TH (CodeQ, ExpQ, Q, bindCode, runIO)
 import Language.Haskell.TH.Syntax (Lift (liftTyped), unTypeCode)
 
--- | A random value generator .
-type Random a = GenIO -> IO a
+-- | A random value generator in the `IO` Monad.
+type RandomIO a = GenIO -> IO a
 
 -- | Yields a random `a` in the quotation monad.
-mkRandomQ :: Random a -> Q a
+mkRandomQ :: RandomIO a -> Q a
 mkRandomQ = runIO . withSystemRandom
 
 -- | Generates a random `a` in compile-time.
-mkRandom :: Lift a => Random a -> CodeQ a
+mkRandom :: Lift a => RandomIO a -> CodeQ a
 mkRandom r = bindCode (mkRandomQ r) liftTyped
 
 -- | Generates an untyped random `a` in compile-time.
-mkRandomE :: Lift a => Random a -> ExpQ
+mkRandomE :: Lift a => RandomIO a -> ExpQ
 mkRandomE = unTypeCode . mkRandom
 
 -- | Generates a randomized literal `Word64` at compile time.
+--
+-- >>> $$mkWord64
+-- 5802473051628008271
 mkWord64 :: CodeQ Word64
 mkWord64 = mkRandom uniformW64
