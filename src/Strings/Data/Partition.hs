6f82d4a966edd1df8164206fe51f2d265367b265
Tiago de Paula <tiagodepalves@gmail.com>
2023-08-11 03:09:20 -0300

    feat(partition): add module Data.Partition
    Removes unused module Data.Pair and replaces it with a Data.Partition,
    that deals with partitions of Strings.

---
+++ src/Strings/Data/Partition.hs
@@ -0,0 +1,48 @@
+module Strings.Data.Partition (
+    Partition,
+    concat,
+    chars,
+    randomShuffle,
+    shuffled,
+    shuffledPartitions,
+) where
+
+import Prelude hiding (String, concat, splitAt)
+
+import Data.Bifunctor (Bifunctor (bimap))
+
+import Strings.Data.String (String, Unbox, concat, replicateM, splitAt)
+import Strings.System.Random (Random, partitions, shuffle, uniformE)
+
+-- | A collection of substring of the same string.
+type Partition a = [String a]
+
+-- | Common constraints for a character.
+type Character a = (Enum a, Bounded a, Unbox a)
+
+-- | Split the `String` in substrings of 1 char each.
+chars :: String a -> Partition a
+chars str
+    | null str = []
+    | otherwise = ch : chars rest
+  where
+    (ch, rest) = splitAt 1 str
+
+-- | Generates a pair of string where one is a simple permutation of the other.
+randomShuffle :: Character a => Int -> Random (String a, String a)
+randomShuffle n = do
+    s1 <- replicateM n uniformE
+    s2 <- shuffle s1
+    pure (s1, s2)
+
+-- | Generates a pair of partitions where one is a simple permutation of the other.
+shuffled :: Character a => Int -> Random (Partition a, Partition a)
+shuffled n = do
+    str <- replicateM n uniformE
+    p1 <- partitions str
+    p2 <- shuffle p1
+    pure (p1, p2)
+
+-- | Generates a pair of strings where one is a permutation of random partitions from the other.
+shuffledPartitions :: Character a => Int -> Random (String a, String a)
+shuffledPartitions n = bimap concat concat <$> shuffled n
