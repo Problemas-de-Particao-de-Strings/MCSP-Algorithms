91df279f8c4879cc180c452ad0fd2ead5cfe62d9
Tiago de Paula <tiagodepalves@gmail.com>
2023-08-23 23:39:35 -0300

    feat(radix-tree): insertion and construction
    Adds 'insert' and 'construct' for 'RadixTree' and 'RadixTreeMap'.

--- src/Strings/Data/RadixTree.hs
+++ src/Strings/Data/RadixTree.hs
@@ -8,12 +8,19 @@ module Strings.Data.RadixTree (
 
     -- * Construction
     empty,
+    construct,
 
     -- * Query
     member,
+
+    -- * Modification
+    insert,
 ) where
 
+import Control.Arrow ((&&&))
 import Data.Bool (Bool)
+import Data.Function (id, (.))
+import Data.List (map)
 import Data.Ord (Ord)
 
 import Strings.Data.RadixTree.Map qualified as Map
@@ -29,6 +36,16 @@ type RadixTree a = Map.RadixTreeMap a (String a)
 empty :: RadixTree a
 empty = Map.empty
 
--- | /O(n log r)/ Check if key is present in the tree.
+-- | /O(n log r)/ Check if string is present in the tree.
 member :: Ord a => String a -> RadixTree a -> Bool
 member = Map.member
+
+-- | /O(?)/ Build a radix tree from a list of strings.
+construct :: Ord a => [String a] -> RadixTree a
+construct = Map.construct . map (id &&& id)
+
+-- | /O(?)/ Insert a string in a tree.
+--
+-- If the tree already contains a string equal to the given value, it is replaced with the new value.
+insert :: Ord a => String a -> RadixTree a -> RadixTree a
+insert s = Map.insert s s
