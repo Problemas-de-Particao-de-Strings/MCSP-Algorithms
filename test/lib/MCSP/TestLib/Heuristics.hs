48976016f4877e23064d4090d6b4730232db55f7
Tiago de Paula <tiagodepalves@gmail.com>
2023-09-02 17:47:42 -0300

    feat(test-lib): add heuristics list
    Adds a simple list of all heuristics implemented to be used in testing.

--- test/lib/MCSP/TestLib/Heuristics.hs
+++ test/lib/MCSP/TestLib/Heuristics.hs
@@ -1,14 +1,28 @@
 module MCSP.TestLib.Heuristics (
+    Heuristic,
     testHeuristic,
+    heuristics,
 ) where
 
 import Prelude hiding (String)
 
+import Data.String qualified as Text
+
 import MCSP.Data.String (String)
-import MCSP.Heuristics (Heuristic)
+import MCSP.Heuristics (
+    Heuristic,
+    combine,
+    combineS,
+ )
 
 -- | Test an heuristic against a sample and return the mean length of the resulting partitions.
 testHeuristic :: Heuristic a -> [(String a, String a)] -> Double
 testHeuristic heuristic sample = fromIntegral (sum results) / fromIntegral (length sample)
   where
     results = map (length . fst . uncurry heuristic) sample
+
+heuristics :: Ord a => [(Text.String, Heuristic a)]
+heuristics =
+    [ ("combine", combine),
+      ("combineS", combineS)
+    ]
