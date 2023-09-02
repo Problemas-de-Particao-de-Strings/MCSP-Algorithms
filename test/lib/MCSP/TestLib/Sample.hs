2ab463448007c80febd64764f70ad6bc69a165d7
Tiago de Paula <tiagodepalves@gmail.com>
2023-09-02 04:02:55 -0300

    feat(string-gen): generic implementation
    Generic implementation of 'genStringPair' that works with any Enum type.

--- test/lib/MCSP/TestLib/Sample.hs
+++ test/lib/MCSP/TestLib/Sample.hs
@@ -7,7 +7,7 @@ import Prelude hiding (String)
 
 import MCSP.Data.String (String)
 import MCSP.System.Random (Random)
-import MCSP.TestLib.Random (randomShuffledCharsWithSingletons)
+import MCSP.TestLib.Random (SimpleEnum, randomShuffledCharsWithSingletons)
 
 -- | Parameters to generate a string of integers.
 --
@@ -20,10 +20,17 @@ data StringParameters = StringParameters
     deriving stock (Eq, Show)
 
 -- | Generate a pair of strings of integers using the given parameters.
-genStringPair :: StringParameters -> Random (String Int, String Int)
+--
+-- >>> import MCSP.System.Random (generateWith)
+-- >>> import Data.Word (Word)
+-- >>> generateWith (1,2) (genStringPair (StringParameters 10 2 2)) :: (String Word, String Word)
+-- (0 0 1 3 1 0 2 1 0 1,3 1 0 0 0 0 1 2 1 1)
+genStringPair :: forall a. SimpleEnum a => StringParameters -> Random (String a, String a)
 genStringPair params =
     randomShuffledCharsWithSingletons
         (stringSize params)
-        1
-        (alphabetSize params)
-        [3 .. (2 + nSingletons params)]
+        (fromMinBound 0)
+        (fromMinBound (alphabetSize params - 1))
+        (fromMinBound (alphabetSize params - 1 + nSingletons params))
+  where
+    fromMinBound n = toEnum (fromEnum (minBound :: a) + n)
