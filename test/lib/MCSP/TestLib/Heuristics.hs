9e4b100da28ed4eba8b7489b6df3d3324f231eee
Tiago de Paula <tiagodepalves@gmail.com>
2023-09-03 02:51:14 -0300

    feat(bench): regenerate pairs
    Regenerate a pair of string each time a heuristic is evoked. The pair
    generation is not measured, as it is considered an enviroment of the
    benchmark.

--- test/lib/MCSP/TestLib/Heuristics.hs
+++ test/lib/MCSP/TestLib/Heuristics.hs
@@ -1,6 +1,5 @@
 module MCSP.TestLib.Heuristics (
     Heuristic,
-    testHeuristic,
     heuristics,
 ) where
 
@@ -8,7 +7,6 @@ import Prelude hiding (String)
 
 import Data.String qualified as Text
 
-import MCSP.Data.String (String)
 import MCSP.Heuristics (
     Heuristic,
     combine,
@@ -17,12 +15,6 @@ import MCSP.Heuristics (
  )
 
 import MCSP.TestLib.Heuristics.TH (mkNamedList)
-
--- | Test an heuristic against a sample and return the mean length of the resulting partitions.
-testHeuristic :: Heuristic a -> [(String a, String a)] -> Double
-testHeuristic heuristic sample = fromIntegral (sum results) / fromIntegral (length sample)
-  where
-    results = map (length . fst . uncurry heuristic) sample
 
 -- | List of all heuristics implemented and their names.
 heuristics :: Ord a => [(Text.String, Heuristic a)]
