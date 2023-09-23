7f868bf0ae3e85eab8e7c135a153c36b5e7d58a8
Tiago de Paula <tiagodepalves@gmail.com>
2023-09-23 16:57:10 -0300

    feat: extracting solutions from an edge list

--- src/MCSP/Data/MatchingGraph.hs
+++ src/MCSP/Data/MatchingGraph.hs
@@ -3,23 +3,37 @@ module MCSP.Data.MatchingGraph (
     -- * Finding Edges
     Edge,
     edgeSet,
+
+    -- * Building Solutions
+    Solution,
+    solution,
+    solutions,
 ) where
 
 import Control.Applicative (liftA2, pure)
-import Data.Foldable (foldMap', foldr, length)
-import Data.Function ((.))
+import Data.Bool (Bool, not, (&&))
+import Data.Foldable (foldMap', foldr, length, null)
+import Data.Function (($), (.))
 import Data.Functor (fmap)
 import Data.Int (Int)
-import Data.List.NonEmpty (NonEmpty (..), (<|))
+import Data.IntMap.Strict qualified as IntMap (IntMap, insert, size, toDescList)
+import Data.Interval (Interval, (<=..<))
+import Data.IntervalSet (IntervalSet, insert, intersection)
+import Data.IntervalSet qualified as IntervalSet (null, singleton)
+import Data.List.NonEmpty (NonEmpty (..), unfoldr, (<|))
 import Data.Map qualified as Map (Map, alter, empty, intersectionWith)
-import Data.Maybe (maybe)
-import Data.Ord (Ord)
+import Data.Maybe (Maybe (..), maybe)
+import Data.Monoid (Monoid (..), mempty)
+import Data.Ord (Ord (..))
+import Data.Vector.Generic qualified as Vector (foldl', snoc)
 import Data.Vector.Unboxed (Vector)
 import GHC.IsList (IsList (..))
-import GHC.Num ((-))
+import GHC.Num (fromInteger, (+), (-))
+import GHC.Real (toInteger)
+import Text.Show (Show)
 
-import MCSP.Data.Pair (Pair)
-import MCSP.Data.String (String, unsafeSlice)
+import MCSP.Data.Pair (Pair, both, left, liftP, right, (&&&))
+import MCSP.Data.String (String (..), unsafeSlice)
 
 -- --------------------- --
 -- Edge Set Construction --
@@ -109,3 +123,160 @@ edgeSet :: Ord a => Pair (String a) -> Vector Edge
 edgeSet = foldMap' asList . commonBlockMap
   where
     asList es = fromListN (length es) (toList es)
+
+-- ------------------ --
+-- Building Solutions --
+-- ------------------ --
+
+-- | Indices for a subtring to be used as a block.
+type Block = Interval Index
+
+-- | Creates an interval representing a substring @S[i .. i + n - 1]@.
+--
+-- >>> blockInterval 1 4
+-- Finite 1 <=..< Finite 5
+blockInterval :: Index -> Length -> Block
+blockInterval lo len = extend lo <=..< extend (lo + len)
+  where
+    extend = fromInteger . toInteger
+
+-- | Extract the interval for the associated blocks in each string from an edge.
+--
+-- >>> toBlocks Edge {start=(0, 2), blockLen=5}
+-- (Finite 0 <=..< Finite 5,Finite 2 <=..< Finite 7)
+toBlocks :: Edge -> Pair Block
+toBlocks Edge {start = (l, r), blockLen = k} = (blockInterval l k, blockInterval r k)
+
+-- | The set of all matched regions of string.
+--
+-- Used to avoid overlapping blocks in the final partition.
+type MatchedSet = IntervalSet Index
+
+-- | Checks if a block was already matched in the current solution.
+--
+-- >>> let edge = Edge {start=(0,2), blockLen=5}
+-- >>> blockInterval 0 5 `overlaps` mempty
+-- False
+-- >>> blockInterval 0 5 `overlaps` IntervalSet.singleton (blockInterval 5 10)
+-- False
+-- >>> blockInterval 0 5 `overlaps` IntervalSet.singleton (blockInterval 4 5)
+-- True
+overlaps :: Block -> MatchedSet -> Bool
+interval `overlaps` set = not $ IntervalSet.null (IntervalSet.singleton interval `intersection` set)
+
+-- | Turn an edge into a non-overlapping interval pair.
+--
+-- Returns `Nothing` if any of the interval would overlap.
+--
+-- >>> let edge = Edge {start=(0,2), blockLen=5}
+-- >>> nonOverlappingBlock edge mempty
+-- Just (Finite 0 <=..< Finite 5,Finite 2 <=..< Finite 7)
+-- >>> nonOverlappingBlock edge (IntervalSet.singleton `both` (toBlocks edge))
+-- Nothing
+nonOverlappingBlock :: Edge -> Pair MatchedSet -> Maybe (Pair Block)
+nonOverlappingBlock edge matched =
+    if not (left block `overlaps` left matched) && not (right block `overlaps` right matched)
+        then Just block
+        else Nothing
+  where
+    block = toBlocks edge
+
+-- | A partial solution to the strings partitioning.
+--
+-- Each @(k,v)@ pair in the map represents a block @S[k ... k + v]@ that should be used in the final
+-- solution.
+type IndexedPartition = IntMap.IntMap Length
+
+-- | Insert an edge to the final solution, without checking for overlapping blocks.
+insertInPartition :: Edge -> Pair IndexedPartition -> Pair IndexedPartition
+insertInPartition edge = liftP (`IntMap.insert` blockLen edge) (start edge)
+
+-- | A collection of data used for constructing solutions from an edge set.
+data MatchingInfo = Info
+    { -- | Collection of blocks from non-overlapping edges, used as a solution.
+      partition :: Pair IndexedPartition,
+      -- | Set of ranges matched in each string.
+      matchedSet :: Pair MatchedSet,
+      -- | List of edges not used in the solution.
+      unused :: Vector Edge
+    }
+    deriving stock (Show)
+
+-- | Representation of a trivial solution.
+--
+-- >>> empty
+-- Info {partition = (fromList [],fromList []), matchedSet = (fromList [],fromList []), unused = []}
+empty :: MatchingInfo
+empty = Info {partition = mempty, matchedSet = mempty, unused = []}
+
+-- | Constructs a partial solution from a list of edges, collecting the `unused` edges.
+--
+-- >>> resolve [Edge {start=(0,2), blockLen=5}]
+-- Info {partition = (fromList [(0,5)],fromList [(2,5)]), matchedSet = (fromList [Finite 0 <=..< Finite 5],fromList [Finite 2 <=..< Finite 7]), unused = []}
+resolve :: Vector Edge -> MatchingInfo
+resolve = Vector.foldl' addEdge empty
+  where
+    addEdge Info {..} edge = case nonOverlappingBlock edge matchedSet of
+        -- non overlapping edge, add to solution
+        Just blocks ->
+            Info
+                { partition = insertInPartition edge partition,
+                  matchedSet = liftP insert blocks matchedSet,
+                  unused
+                }
+        -- edge overlaps, add to unused
+        Nothing ->
+            Info
+                { partition,
+                  matchedSet,
+                  unused = Vector.snoc unused edge
+                }
+
+-- | Construct another solution with the `unused` edges of the previous solution.
+--
+-- Return `Nothing` for any trivial solution.
+--
+-- >>> nextSolution (resolve [Edge {start=(0,2), blockLen=5}])
+-- Just (Info {partition = (fromList [],fromList []), matchedSet = (fromList [],fromList []), unused = []})
+-- >>> nextSolution empty
+-- Nothing
+nextSolution :: MatchingInfo -> Maybe MatchingInfo
+nextSolution Info {..} =
+    if null (left partition) && null (right partition)
+        then Nothing
+        else Just (resolve unused)
+
+-- | A complete solution to the strings partitioning.
+--
+-- Each @(k,v)@ pair in the solution represents a block @S[k ... k + v]@.
+type Solution = Pair (Vector (Index, Length))
+
+-- | Extract the solution from the `MatchingInfo`.
+toSolution :: MatchingInfo -> Solution
+toSolution Info {..} = toVector `both` partition
+  where
+    toVector part = fromListN (IntMap.size part) (IntMap.toDescList part)
+
+-- | The solution represented by the edge list.
+--
+-- Apply each edge in the same order they are listed, ignoring edges that overlaps with the partial
+-- solution.
+--
+-- >>> solution $ edgeSet ("abab", "abba")
+-- ([(0,2)],[(0,2)])
+-- >>> solution $ edgeSet ("abab", "abab")
+-- ([(2,2),(0,2)],[(2,2),(0,2)])
+solution :: Vector Edge -> Solution
+solution = toSolution . resolve
+
+-- | List of all solutions represented with an ordering of the edge set.
+--
+-- This is done by reapeatedly constructing `solution`s with the unused edges from the previous
+-- `solution`.
+--
+-- >>> solutions $ edgeSet ("abab", "abba")
+-- ([(0,2)],[(0,2)]) :| [([(2,2)],[(0,2)]),([(1,2)],[(2,2)]),([],[])]
+-- >>> solutions $ edgeSet ("abab", "abab")
+-- ([(2,2),(0,2)],[(2,2),(0,2)]) :| [([(2,2),(0,2)],[(2,2),(0,2)]),([(0,3)],[(0,3)]),([(0,4)],[(0,4)]),([(1,2)],[(1,2)]),([(1,3)],[(1,3)]),([],[])]
+solutions :: Vector Edge -> NonEmpty Solution
+solutions edges = unfoldr (toSolution &&& nextSolution) (resolve edges)
