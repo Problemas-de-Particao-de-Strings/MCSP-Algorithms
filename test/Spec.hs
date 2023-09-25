fdd3bf8e43c620a534e1099ffb7a930550405f1a
Tiago de Paula <tiagodepalves@gmail.com>
2023-09-25 03:40:29 -0300

    feat(tests): increase quickcheck limit
    By default, quickcheck only runs 100 instances. With this change, the
    default is now at 1000 instances.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,14 @@
 module Main (main) where
 
+import Data.Bifunctor (first)
+import Data.Data (Typeable)
 import Data.Function (($))
+import Data.Functor ((<$>))
 import System.IO (IO)
 
-import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty (TestTree, askOption, defaultMain, localOption, testGroup)
+import Test.Tasty.Options (IsOption (..))
+import Test.Tasty.QuickCheck (QuickCheckTests (..))
 
 import MCSP.Tests.DocTest (testDocs)
 import MCSP.Tests.RadixTree (radixTreeTests)
@@ -12,9 +17,25 @@ import MCSP.Tests.StringExtra (stringExtraTests)
 main :: IO ()
 main =
     defaultMain $
-        testGroup
-            "Tests"
-            [ radixTreeTests,
-              stringExtraTests,
-              testDocs "src"
-            ]
+        modifyDefault $
+            testGroup
+                "Tests"
+                [ radixTreeTests,
+                  stringExtraTests,
+                  testDocs "src"
+                ]
+
+newtype ModifiedQuickCheckTests = Tests QuickCheckTests
+    deriving newtype (Typeable)
+
+instance IsOption ModifiedQuickCheckTests where
+    defaultValue = Tests (QuickCheckTests 1_000)
+    parseValue text = Tests <$> parseValue text
+    optionName = first Tests optionName
+    optionHelp = first Tests optionHelp
+    showDefaultValue (Tests t) = showDefaultValue t
+    optionCLParser = Tests <$> optionCLParser
+
+modifyDefault :: TestTree -> TestTree
+modifyDefault tests = askOption $ \(Tests n) ->
+    localOption n tests
