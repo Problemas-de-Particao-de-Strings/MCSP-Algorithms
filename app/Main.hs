422c03cd04ca87223dce9981a9b929701d93ae71
Tiago de Paula <tiagodepalves@gmail.com>
2023-10-07 14:51:24 -0300

    refactor(bench): remove seed from CSV

--- app/Main.hs
+++ app/Main.hs
@@ -6,12 +6,12 @@ import Control.Monad (forM_, replicateM_)
 import Data.Word (Word8)
 
 import MCSP.Data.String (String)
-import MCSP.System.Random (Random, randomSeed, uniformR)
+import MCSP.System.Random (Random, generate, uniformR)
 import MCSP.TestLib.Heuristics (csvHeader, heuristics, measure, toCsvRow)
 import MCSP.TestLib.Sample (ShuffleMethod (..), StringParameters (..), randomPairWith)
 
-genPair :: Random (String Word8, String Word8)
-genPair = do
+randomPair :: Random (String Word8, String Word8)
+randomPair = do
     r <- uniformR 1 10
     s <- uniformR 2 50
     n <- uniformR (r + s + 10) 200
@@ -25,9 +25,9 @@ genPair = do
 
 run :: IO ()
 run = do
-    seed <- randomSeed
+    pair <- generate randomPair
     forM_ heuristics $ \heuristc ->
-        putStrLn $ toCsvRow $ measure heuristc seed genPair
+        putStrLn $ toCsvRow $ measure heuristc pair
 
 main :: IO ()
 main = putStrLn csvHeader >> replicateM_ 10_000 run
