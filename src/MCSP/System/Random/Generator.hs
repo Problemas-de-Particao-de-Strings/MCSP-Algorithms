87f3e3948c4fc26743a73461b01665376fd54092
Tiago de Paula <tiagodepalves@gmail.com>
2023-10-14 16:31:38 -0300

    feat(random): "true" randomness

--- src/MCSP/System/Random/Generator.hs
+++ src/MCSP/System/Random/Generator.hs
@@ -4,20 +4,31 @@ module MCSP.System.Random.Generator (
     GenPCG,
 ) where
 
-import Control.Monad (Monad (..))
+import Control.Applicative (pure)
+import Control.Monad (Monad (..), fail)
 import Control.Monad.Primitive (PrimMonad, PrimState)
-import Data.Bits (finiteBitSize, shift, (.&.))
+import Data.Bits (FiniteBits, finiteBitSize, shift, (.&.))
+import Data.Bool (otherwise)
+import Data.Eq (Eq (..))
 import Data.Function (id)
 import Data.Functor ((<$>))
+import Data.Int (Int)
+import Data.Maybe (Maybe (..), maybe)
+import Data.Ord (Ord (..))
+import Data.Store (decodeIO)
 import Data.Tuple (uncurry)
 import Data.Tuple.Extra (both)
 import Data.Type.Equality (type (~))
 import Data.Vector.Unboxed (Vector)
-import Data.Word (Word32, Word64)
-import GHC.Enum (maxBound)
-import GHC.Real (fromIntegral)
+import Data.Word (Word32, Word64, Word8)
+import GHC.Base (asTypeOf, ($!))
+import GHC.Enum (Bounded, maxBound)
+import GHC.Num ((*), (+), (-))
+import GHC.Real (Integral, div, fromIntegral)
 import System.IO (IO)
+import Text.Show (Show)
 
+import System.Entropy (getEntropy, getHardwareEntropy)
 import System.Random.MWC qualified (Gen, initialize, uniformM, uniformRM)
 import System.Random.PCG qualified (Gen, initialize)
 import System.Random.PCG.Class qualified as PCG (uniform1, uniform1B, uniform2)
@@ -177,4 +188,62 @@ instance Generator GenPCGUnique IO where
     uniform2 = PCG.uniform2 (,)
     {-# INLINE uniform2 #-}
     uniform1B = PCG.uniform1B id
+    {-# INLINE uniform1B #-}
+
+uniformRange :: (Integral a, Bounded a, Monad m) => (a, a) -> m a -> m a
+uniformRange (x1, x2) genUniform
+    | n == 0 = genUniform -- Abuse overflow in unsigned types
+    | otherwise = loop
+  where
+    -- Allow ranges where x2<x1
+    (i, j)
+        | x1 < x2 = (x1, x2)
+        | otherwise = (x2, x1)
+    n = 1 + (j - i)
+    buckets = maxBound `div` n
+    maxN = buckets * n
+    loop = do
+        x <- genUniform
+        if x < maxN
+            then pure $! i + x `div` buckets
+            else loop
+{-# INLINE uniformRange #-}
+
+-- | Gets the size in bytes for type @a@.
+--
+-- >>> sizeOf (0 :: Word32)
+-- 4
+sizeOf :: (FiniteBits a, Bounded a) => a -> Int
+sizeOf val = finiteBitSize (asTypeOf maxBound val) `div` finiteBitSize (maxBound :: Word8)
+{-# INLINE sizeOf #-}
+
+data GenEntropy = GenEntropy
+    deriving stock (Eq, Ord, Show)
+
+instance Generator GenEntropy IO where
+    type Seed GenEntropy = GenEntropy
+    initialize = pure
+    {-# INLINE initialize #-}
+    uniform1 GenEntropy = getEntropy (sizeOf (0 :: Word32)) >>= decodeIO
+    {-# INLINE uniform1 #-}
+    uniform2 GenEntropy = getEntropy (sizeOf (0 :: Word64)) >>= decodeIO
+    {-# INLINE uniform2 #-}
+    uniform1B hi gen = uniformRange (0, hi) (uniform1 gen)
+    {-# INLINE uniform1B #-}
+
+data GenHWEntropy = GenHWEntropy
+    deriving stock (Eq, Ord, Show)
+
+unwrapIO :: Maybe a -> IO a
+unwrapIO = maybe (fail "GenHWEntropy: no hardware entropy available") pure
+
+instance Generator GenHWEntropy IO where
+    type Seed GenHWEntropy = GenHWEntropy
+    initialize = pure
+    {-# INLINE initialize #-}
+    uniform1 GenHWEntropy = getHardwareEntropy (sizeOf (0 :: Word32)) >>= unwrapIO >>= decodeIO
+    {-# INLINE uniform1 #-}
+    uniform2 GenHWEntropy = getHardwareEntropy (sizeOf (0 :: Word64)) >>= unwrapIO >>= decodeIO
+    {-# INLINE uniform2 #-}
+    uniform1B hi gen = uniformRange (0, hi) (uniform1 gen)
     {-# INLINE uniform1B #-}
