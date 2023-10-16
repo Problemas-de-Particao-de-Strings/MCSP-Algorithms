d801ef04d9aa9d5ad227efada92e2a2bb369d870
Tiago de Paula <tiagodepalves@gmail.com>
2023-10-15 21:15:27 -0300

    feat(random): lazy and infinite random values

--- src/MCSP/System/Random/Monad.hs
+++ src/MCSP/System/Random/Monad.hs
@@ -3,10 +3,13 @@ module MCSP.System.Random.Monad (
     Random,
     evalRandom,
     liftRandom,
+    lazyRandom,
 ) where
 
 import Control.Applicative (Applicative (..))
 import Control.Monad (Monad (..))
+import Control.Monad.ST (ST)
+import Control.Monad.ST.Unsafe (unsafeInterleaveST)
 import Data.Function (const, ($))
 import Data.Functor (Functor (..), (<$>))
 import Data.Monoid (Monoid (..))
@@ -19,10 +22,10 @@ import System.Random.PCG.Class (Generator)
 -- ------------ --
 
 -- | A monad capable of producing random values of @a@.
-newtype Random a = Random (forall g m. Generator g m => g -> m a)
+newtype Random a = Random (forall g s. Generator g (ST s) => g -> ST s a)
 
 instance Functor Random where
-    fmap f (Random gena) = liftRandom $ \rng -> do
+    fmap f (Random gena) = Random $ \rng -> do
         a <- gena rng
         pure (f a)
     {-# INLINE fmap #-}
@@ -32,28 +35,28 @@ instance Functor Random where
 instance Applicative Random where
     pure x = Random (const (pure x))
     {-# INLINE pure #-}
-    liftA2 f (Random gena) (Random genb) = liftRandom $ \rng -> do
+    liftA2 f (Random gena) (Random genb) = Random $ \rng -> do
         a <- gena rng
         b <- genb rng
         pure (f a b)
     {-# INLINE liftA2 #-}
-    Random genf <*> Random gena = liftRandom $ \rng -> do
+    Random genf <*> Random gena = Random $ \rng -> do
         f <- genf rng
         a <- gena rng
         pure (f a)
     {-# INLINE (<*>) #-}
-    Random gena *> Random genb = liftRandom $ \rng -> do
+    Random gena *> Random genb = Random $ \rng -> do
         _ <- gena rng
         genb rng
     {-# INLINE (*>) #-}
-    Random gena <* Random genb = liftRandom $ \rng -> do
+    Random gena <* Random genb = Random $ \rng -> do
         a <- gena rng
         _ <- genb rng
         pure a
     {-# INLINE (<*) #-}
 
 instance Monad Random where
-    Random gena >>= f = liftRandom $ \rng -> do
+    Random gena >>= f = Random $ \rng -> do
         a <- gena rng
         evalRandom (f a) rng
     {-# INLINE (>>=) #-}
@@ -77,11 +80,22 @@ instance Monoid a => Monoid (Random a) where
 -- ---------- --
 
 -- | Evaluate a random computation with the given initial generator and return the final value.
-evalRandom :: Generator g m => Random a -> g -> m a
+evalRandom :: Generator g (ST s) => Random a -> g -> ST s a
 evalRandom (Random gen) = gen
 {-# INLINE evalRandom #-}
 
 -- | Turn a standard RNG function into a `Random` monad.
 liftRandom :: (forall g m. Generator g m => g -> m a) -> Random a
-liftRandom = Random
+liftRandom gen = Random $ \rng -> gen rng
 {-# INLINE liftRandom #-}
+
+-- | Allows a `Random` monad to be evaluated lazily.
+--
+-- This function should be applied with care, otherwise a strict `Random` monad could still force
+-- early evaluation of this lazy version.
+lazyRandom :: Random a -> Random a
+lazyRandom (Random gen) = Random $ \rng ->
+    -- SAFETY: unsafeInterleaveST break the order of ST operations, which shouldn't matter for
+    -- random operations (except of changing the output value)
+    unsafeInterleaveST (gen rng)
+{-# INLINE lazyRandom #-}
