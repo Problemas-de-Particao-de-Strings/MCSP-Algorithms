4cc9d44be7d361f509889e33740dc4e5c82997b3
Tiago de Paula <tiagodepalves@gmail.com>
2023-10-19 00:44:43 -0300

    feat(pso): implicit parameters for the heuristic

--- src/MCSP/Heuristics/PSOBased.hs
+++ src/MCSP/Heuristics/PSOBased.hs
@@ -30,7 +30,7 @@ import MCSP.Data.MatchingGraph (Edge, edgeSet, mergeness, solution, toPartitions
 import MCSP.Data.Pair (Pair)
 import MCSP.Data.String (String)
 import MCSP.Data.String.Extra (Partition)
-import MCSP.System.Random (Random, generateWith)
+import MCSP.System.Random (Random, Seed, generateWith)
 
 -- | Default updater consider local best, global best and random components.
 defaultUpdater :: Updater Edge
@@ -42,38 +42,42 @@ defaultUpdater =
               weighted 0.005 globalGuideDirection
             ]
 
--- | Number of iterations in the particle swarm optimization.
-maximumIterations :: Int
-maximumIterations = 10
-
--- | Number of particles used for each swarm.
-numberOfParticles :: Int
-numberOfParticles = 1000
-
 -- --------- --
 -- Heuristic --
 -- --------- --
 
+-- | Some meta-parameters for the PSO Heuristic.
+type PSOParams =
+    ( ?iterations :: Int,
+      ?particles :: Int,
+      ?seed :: Seed
+    )
+
 -- | Create an iterated PSO swarm for the MCSP problem.
-mcspSwarm :: Ord a => Int -> Int -> Pair (String a) -> Random [Swarm Edge]
-mcspSwarm iterations particles (edgeSet -> edges) =
-    take iterations <$> particleSwarmOptimization defaultUpdater ?initialWeights particles
+mcspSwarm :: (Ord a, PSOParams) => Pair (String a) -> Random [Swarm Edge]
+mcspSwarm (edgeSet -> edges) =
+    take ?iterations <$> particleSwarmOptimization defaultUpdater ?initialWeights ?particles
   where
     ?eval = fromIntegral . mergeness . solution
     ?values = edges
     ?initialWeights = randomWeights (length edges)
 
--- | Monadic PSO heuristic.
-psoM :: Ord a => Pair (String a) -> Random (Pair (Partition a))
-psoM strs = do
-    swarms <- mcspSwarm maximumIterations numberOfParticles strs
+-- | PSO heuristic with implicit parameters.
+psoWithParams :: (Ord a, PSOParams) => Pair (String a) -> Pair (Partition a)
+psoWithParams strs = generateWith ?seed $ do
+    swarms <- mcspSwarm strs
     case reverse swarms of
         swarm : _ -> pure $ toPartitions strs $ solution $ sortedValues $ gGuide swarm
         [] -> error "pso: mcspSwarm generated no swarms"
 
+-- ------------------ --
+-- Default Parameters --
+
 -- | PSO heuristic.
 pso :: Ord a => Pair (String a) -> Pair (Partition a)
-pso strs = generateWith seed (psoM strs)
+pso = psoWithParams
   where
+    ?iterations = 10
+    ?particles = 1000
     -- from https://www.random.org
-    seed = (0x7f166a5f52178da7, 0xe190ca41e26454c3)
+    ?seed = (0x7f166a5f52178da7, 0xe190ca41e26454c3)
