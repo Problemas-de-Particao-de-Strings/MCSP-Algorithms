d28b04053ca8d5d81ffc0612ee9f2f7d9b5a2e79
Tiago de Paula <tiagodepalves@gmail.com>
2023-12-14 01:06:01 -0300

    feat(meta): simplified type-map implementation

--- src/MCSP/Data/Meta.hs
+++ src/MCSP/Data/Meta.hs
@@ -4,8 +4,6 @@ module MCSP.Data.Meta (
 
     -- * Collection of variables
     VariableMap,
-    empty,
-    (<:),
     lookup,
 
     -- * Monadic operations
@@ -16,37 +14,43 @@ module MCSP.Data.Meta (
     getVarOrDefault,
 
     -- ** Execution
-    evalMetaWith,
-    runMetaWith,
     evalMeta,
     runMeta,
 ) where
 
 import Control.Applicative (Applicative)
-import Control.Monad (Monad, (>>), (>>=))
+import Control.Monad (Monad, (>>))
 import Control.Monad.Trans.State.Strict (State, evalState, gets, modify, runState, state)
-import Data.Dynamic (Dynamic, fromDynamic, toDyn)
-import Data.Function (($), (.))
+import Data.Function (id, ($), (.))
 import Data.Functor (Functor (..))
-import Data.Map.Strict qualified as Map (Map, empty, insert, keys, lookup)
 import Data.Maybe (Maybe (..))
-import Data.Proxy (Proxy (..))
-import Data.Typeable (TypeRep, Typeable, typeOf, typeRep)
-import Text.Show (Show (..), showChar, showString)
+import Data.TypeMap.Dynamic.Alt qualified as Map (Item, TypeMap, empty, insert, lookup, map, toList)
+import Data.Typeable (Typeable, showsTypeRep, typeOf)
+import Text.Show (Show (..), showChar, showListWith, showString)
 
 -- | Represents a additional variable that can be used as input or output of a computation.
 --
 -- These variables are set and resolved dynamically inside the `Meta` monad.
 class Typeable v => MetaVariable v
 
+-- ---------- --
+-- Collection --
+
+-- | A fully dynamic `TypeMap` mapping from a type to itself.
+data Dynamic
+
+type instance Map.Item Dynamic v = v
+
 -- | A polymorphic storage of `MetaVariable`s.
-newtype VariableMap = VariableMap (Map.Map TypeRep Dynamic)
+newtype VariableMap = VariableMap (Map.TypeMap Dynamic)
 
 instance Show VariableMap where
-    showsPrec d (VariableMap vars) =
+    showsPrec _ (VariableMap vars) =
         showString "VariableMap"
             . showChar ' '
-            . showsPrec d (Map.keys vars)
+            . showListWith id keys
+      where
+        keys = Map.toList $ Map.map (showsTypeRep . typeOf) vars
 
 -- | A map with no variables set.
 --
@@ -55,33 +59,6 @@ instance Show VariableMap where
 empty :: VariableMap
 empty = VariableMap Map.empty
 {-# INLINEABLE empty #-}
-
--- | Insert a meta-variable into the map.
---
--- >>> import Prelude (Int)
--- >>> instance MetaVariable Int
---
--- >>> insert @Int 12 empty
--- VariableMap [Int]
-insert :: MetaVariable v => v -> VariableMap -> VariableMap
-insert val (VariableMap vars) =
-    VariableMap $ Map.insert (typeOf val) (toDyn val) vars
-{-# INLINEABLE insert #-}
-
--- | Insert a meta-variable into the map.
---
--- Infix version of `insert`.
---
--- >>> import Prelude (Int, Double, String)
--- >>> instance MetaVariable Int
--- >>> instance MetaVariable Double
--- >>> instance MetaVariable String
---
--- >>> empty <: (12 :: Int) <: (3.14 :: Double) <: "Hello"
--- VariableMap [Double,Int,[Char]]
-(<:) :: MetaVariable v => VariableMap -> v -> VariableMap
-vars <: val = insert val vars
-{-# INLINEABLE (<:) #-}
 
 -- | Return `Just` the value a `MetaVariable` of the given type, or `Nothing` if no such variable is
 -- available.
@@ -92,10 +69,11 @@ vars <: val = insert val vars
 -- >>> lookup @Int empty
 -- Nothing
 --
--- >>> lookup @Int (empty <: (12 :: Int))
+-- >>> let (_, vars) = runMeta (setVar (12 :: Int))
+-- >>> lookup @Int vars
 -- Just 12
-lookup :: forall v. MetaVariable v => VariableMap -> Maybe v
-lookup (VariableMap vars) = Map.lookup (typeRep (Proxy @v)) vars >>= fromDynamic
+lookup :: MetaVariable v => VariableMap -> Maybe v
+lookup (VariableMap vars) = Map.lookup vars
 {-# INLINEABLE lookup #-}
 
 -- | Return the previous value of a `MetaVariable` of the given type, or insert and return the
@@ -107,13 +85,19 @@ lookup (VariableMap vars) = Map.lookup (typeRep (Proxy @v)) vars >>= fromDynamic
 -- >>> lookupOrInsert (12 :: Int) empty
 -- (12,VariableMap [Int])
 --
--- >>> lookupOrInsert (12 :: Int) $ empty <: (34 :: Int)
+-- >>> let (_, vars) = runMeta (setVar (34 :: Int))
+-- >>> lookupOrInsert (12 :: Int) vars
 -- (34,VariableMap [Int])
 lookupOrInsert :: MetaVariable v => v -> VariableMap -> (v, VariableMap)
 lookupOrInsert defaultValue vars = case lookup vars of
     Just value -> (value, vars)
     Nothing -> (defaultValue, insert defaultValue vars)
+  where
+    insert value (VariableMap vm) = VariableMap $ Map.insert value vm
 {-# INLINEABLE lookupOrInsert #-}
+
+-- ----------------- --
+-- Monadic Operation --
 
 -- | A monad that represents operation with `MetaVariable`s.
 --
@@ -129,7 +113,9 @@ newtype Meta a = Meta (State VariableMap a)
 -- >>> runMeta (setVar (12 :: Int))
 -- ((),VariableMap [Int])
 setVar :: MetaVariable v => v -> Meta ()
-setVar value = Meta $ modify (insert value)
+setVar value = Meta $ modify $ variableMap (Map.insert value)
+  where
+    variableMap f (VariableMap vm) = VariableMap (f vm)
 {-# INLINEABLE setVar #-}
 
 -- | Set a meta-variable for the given type.
@@ -173,22 +159,12 @@ getVarOrDefault :: MetaVariable v => v -> Meta v
 getVarOrDefault value = Meta $ state (lookupOrInsert value)
 {-# INLINEABLE getVarOrDefault #-}
 
--- | Execute a `Meta` monad with the given variables and return the output.
-evalMetaWith :: VariableMap -> Meta a -> a
-evalMetaWith vars (Meta m) = evalState m vars
-{-# INLINEABLE evalMetaWith #-}
-
 -- | Execute a `Meta` monad and return the output.
 evalMeta :: Meta a -> a
-evalMeta = evalMetaWith empty
+evalMeta (Meta m) = evalState m empty
 {-# INLINEABLE evalMeta #-}
-
--- | Execute a `Meta` monad with the given variables and return the output and the final variables.
-runMetaWith :: VariableMap -> Meta a -> (a, VariableMap)
-runMetaWith vars (Meta m) = runState m vars
-{-# INLINEABLE runMetaWith #-}
 
 -- | Execute a `Meta` monad and return the output and the final variables.
 runMeta :: Meta a -> (a, VariableMap)
-runMeta = runMetaWith empty
+runMeta (Meta m) = runState m empty
 {-# INLINEABLE runMeta #-}
