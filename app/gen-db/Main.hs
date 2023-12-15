1741c631ddefdcb657cf740b833b7a5be5a013b7
Tiago de Paula <tiagodepalves@gmail.com>
2023-12-15 01:20:45 -0300

    feat(gen-db): initial implementation

--- app/gen-db/Main.hs
+++ app/gen-db/Main.hs
@@ -1,17 +1,32 @@
 module Main (main) where
 
-import Prelude hiding (String)
-
-import Control.Monad (forM_, replicateM_)
-import Data.Data (Proxy (..))
+import Control.Applicative (pure, (<$>), (<**>))
+import Control.Monad (forM_, replicateM, (>>))
+import Data.Foldable (length)
+import Data.Function (($))
+import Data.List (sortOn)
+import Data.Monoid ((<>))
+import Data.Ord (Ord (..))
 import Data.Word (Word8)
-import System.IO (hFlush, stdout)
+import GHC.Num (Num (..))
+import GHC.Show (Show (..))
+import Options.Applicative.Builder (fullDesc, header, info, progDesc)
+import Options.Applicative.Common (ParserInfo)
+import Options.Applicative.Extra (execParser, helper)
+import System.IO (IO, hFlush, putStrLn, stdout)
 
-import MCSP.Data.String (String)
+import MCSP.Data.String (String, replicate)
 import MCSP.System.Random (Random, generate, uniformR)
-import MCSP.TestLib.Heuristics (Measured, heuristics, measure)
 import MCSP.TestLib.Sample (ShuffleMethod (..), SimpleEnum, StringParameters (..), randomPairWith)
-import MCSP.Text.CSV (headers, row)
+
+args :: ParserInfo ()
+args =
+    info (options <**> helper) $
+        fullDesc
+            <> header "gen-db - generate string pairs for the MCSP problem"
+            <> progDesc "Generate 1000 pairs with predefined parameters"
+  where
+    options = pure ()
 
 randomPair :: SimpleEnum a => Random (String a, String a)
 randomPair = do
@@ -26,13 +41,18 @@ randomPair = do
               shuffle = Chars
             }
 
-main :: IO ()
-main = do
-    putLn $ headers (Proxy @Measured)
-    replicateM_ 10_000 $ do
-        pair <- generate randomPair
-        forM_ heuristics $ \heuristc -> do
-            result <- measure @Word8 heuristc pair
-            putLn $ row result
+run :: IO ()
+run = do
+    strs <- sortOn strSize <$> replicateM 1_000 (generate (randomPair @Word8))
+    forM_ strs $ \(s1, s2) -> do
+        let n = length s1 `max` length s2
+        putLn $ show s1
+        putLn $ show $ replicate @Word8 (n + 1) 0
+        putLn $ show s2
+        putLn $ show $ replicate @Word8 (n + 3) 0
   where
+    strSize (s1, s2) = length s1 + length s2
     putLn line = putStrLn line >> hFlush stdout
+
+main :: IO ()
+main = execParser args >> run
